85fcdc93dfe9f1a406ad985d9801fed1412abb0c
--global <kakaxizhh@gmail.com>
2025-06-18 22:08:52 +0800

    feat: add shadcn ui

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,8 @@
 import config from "@repo/eslint-config/next";
+import { globalIgnores } from "eslint/config";
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default [
+  globalIgnores(["src/components/ui/**/*", "eslint.config.mjs"]),
+  ...config
+];
